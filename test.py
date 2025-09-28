d1c58d40b560fa4e581ca2a9fd613a21c8ed1d9f
Krishna28535 <krishnakesharwani28535@gmail.com>
2025-09-28 22:26:55 +0530

    Remove test.py from repository, now ignored by .gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-from exception.exception import HolidayPlannerException
-try:
-    a=int(input("Enter number: "))
-    b=int(input("Enter number: "))
-    print(a/b)
-except ValueError as e:
-    raise HolidayPlannerException(e)
